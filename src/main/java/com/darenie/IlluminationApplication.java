d7647bb705313dd51d8a07bf2379c57650307462
matewydm <mucha_303@outlook.com>
2017-05-14 15:37:16 +0200

    wstepna konfiguracja serwera

---
+++ src/main/java/com/darenie/IlluminationApplication.java
@@ -0,0 +1,12 @@
+package com.darenie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlluminationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlluminationApplication.class, args);
+	}
+}
