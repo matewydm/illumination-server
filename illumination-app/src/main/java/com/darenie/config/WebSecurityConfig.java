edca8c42fa312b1ad342b67ba0a80966af42ae67
mkaldus <mkaldus12@gmail.com>
2017-05-27 15:15:48 +0200

    styles

--- illumination-app/src/main/java/com/darenie/config/WebSecurityConfig.java
+++ illumination-app/src/main/java/com/darenie/config/WebSecurityConfig.java
@@ -35,7 +35,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login")
+                .loginPage("/login").defaultSuccessUrl("/")
                 .permitAll()
                 .and()
                 .logout()
