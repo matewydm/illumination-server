009aa4d7c1cb96d2304ad77f1bd61dedbde67ae8
matewydm <mucha_303@outlook.com>
2017-06-02 01:18:53 +0200

    proba przekazania parametru

--- illumination-view/src/main/java/com/darenie/controllers/ViewController.java
+++ illumination-view/src/main/java/com/darenie/controllers/ViewController.java
@@ -2,11 +2,18 @@ package com.darenie.controllers;
 
 import com.darenie.json.model.LightLampDataJson;
 import com.darenie.service.LightLampService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.ServletRequest;
 import java.util.List;
@@ -34,15 +41,21 @@ public class ViewController {
         return "login";
     }
 
-    @RequestMapping("/map")
-    public String map(){
-        return "map";
-    }
+    @RequestMapping(value = "/map", method = RequestMethod.GET)
+    public ModelAndView lampMap(){
 
-    @RequestMapping("/lamps")
-    public String lamps(){
+        ModelAndView model = new ModelAndView("map");
+        ObjectMapper mapper = new ObjectMapper();
+
         List<LightLampDataJson> lightLampDataJsons = lightLampService.getAll();
 
-        return "dare";
+        try {
+            String jsonString = mapper.writeValueAsString(lightLampDataJsons);
+            model.addObject("lightMapList", jsonString);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return model;
     }
 }
